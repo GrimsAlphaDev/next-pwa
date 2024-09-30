ec2e89b5e37e67305fc12e4853b5786fc47a00f9
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-09-30 16:32:58 +0700

    initial

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+    dest: "public",
+});
+
+export default withPWA({
+    // Your Next.js config
+});
+
+/** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
